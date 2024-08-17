d3680057dad08c79f0d3cb30b31ac933fe15960a
Lucas Rakotomalala <lucas.rakotomalala@yahoo.fr>
2024-08-17 17:47:23 +0200

    chore: remove logs and set default difficulty to `1`

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # Porquerolles Puzzle
+
 Porquerolles Puzzle for Marion L. birthday
 
 ## Puzzle
+
 The puzzle is a 12 pieces puzzle with a picture of Porquerolles island.
 
+## Difficulty
+
+The puzzle's difficulty is set to `1` by default.
+It can be changed by set to `2` `difficulty=2` to the query params of URL.
+`difficulty` should be a number between `1` and `5`.
+
 ## Solution
-The puzzle can be solved at loading by adding `?cheat=true` to the URL.
+
+The puzzle can be solved at loading by adding `cheat=true` or `cheat=1` to the query params of URL.
 
 ## Credits
 
--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,30 @@
 'use strict';
 
 const urlParams = new URLSearchParams(window.location.search);
-const difficulty = urlParams.get('difficulty') || 2;
-const cheat = urlParams.get('cheat') || false;
-console.log(difficulty, cheat);
+const difficulty = urlParams.get('difficulty');
+const cheat = urlParams.get('cheat') === 'true' || urlParams.get('cheat') === '1';
 
 const game = new Puzzle({
-  tileSize: 120, // width and height of the tile
+  tileSize: 120,
   columns: 3,
   rows: 4,
-  difficulty: Number(difficulty),
-  scale: 0.992, // Determines the scale of the tiles. As scale value less than 1 reveals gridlines
+  difficulty: Number(difficulty) || 1,
+  scale: 0.994,
   image: {
     url: './images/puzzle.jpg',
-    preserve: 'width', //['width', 'height'] the width or height will be preserved
+    preserve: 'width',
     offset: 0
   }
 });
 
 game.render('puzzle');
 
-if (cheat !== 'true') { 
+if (!cheat) { 
   game.start();
 
   game.onSolve((_event) => {
-    alert('Bien joué! Tu as remporté la première épreuve! Voici la carte complète.');
+    alert('Bien joué! Tu as remporté la première épreuve!\nVoici la carte complète.');
   });
+} else {
+  alert('Mode triche activé!\nVoici la carte complète.');
 }
